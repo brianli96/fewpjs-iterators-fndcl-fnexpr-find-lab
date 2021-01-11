9ff67989c66837083e69ccab564f58c02499283d
Brian Li <Brian.Li96@yahoo.com>
2021-01-11 15:38:50 -0600

    commit tree d9e8e7a84e275c8328446af0604927c0a830ea98

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+superbowlWin = (array) => 
+{
+  let result = array.find(elem => elem.result === 'W');
+  if(result)
+  {
+    return result.year;
+  }
+  else
+  {
+    return undefined;
+  }
+}
